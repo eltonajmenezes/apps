027f08ca014b6dabdb86df6211343c571021f011
eltonajmenezes <66936537+eltonajmenezes@users.noreply.github.com>
2021-06-18 00:00:46 +0530

    Update combinedscript2.sh

--- combinedscript2.sh
+++ combinedscript2.sh
@@ -3317,7 +3317,7 @@ microsoftteams)
     type="pkg"
     #packageID="com.microsoft.teams"
     downloadURL="https://go.microsoft.com/fwlink/?linkid=869428"
-#    appNewVersion=$(curl -fs https://macadmins.software/latest.xml | xpath '//latest/package[id="com.microsoft.teams.standalone"]/version' 2>/dev/null | sed -E 's/<version>([0-9.]*) .*/\1/')
+    appNewVersion=$(curl -fs https://macadmins.software/latest.xml | xpath '//latest/package[id="com.microsoft.teams.standalone"]/version' 2>/dev/null | sed -E 's/<version>([0-9.]*) .*/\1/')
     # Still using macadmin.software for version, as the path does not contain the version in a matching format. packageID can be used, but version is the same.
     expectedTeamID="UBF8T346G9"
     blockingProcesses=( "Microsoft Teams" "Microsoft Teams Helper" "Teams" )
